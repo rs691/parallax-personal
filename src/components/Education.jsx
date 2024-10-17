4f12cbf491c82bdd2eb152790d589d7acf5235fc
rs691 <robstewart@my365.bellvue.edu>
2024-10-17 01:17:01 -0500

    add first

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,21 +1,27 @@
-import '../App.css';
+
 import styles from './ContentPages.module.css';
+
 import logo from '../assets/logo.png';
 import horizontal from '../assets/horizontal.png';
 
 function Education() {
   return (
     <div>
-    
-        <a href="https://www.iwcc.edu/" target="_blank">
+    <div className={styles.education}>
+
+<h1>Education</h1>
+
+     
+
+      
+      
           <img src={logo} className="logo" alt="pirate logo" />
-        </a>
-        <a href="https://www.bellevue.edu/" target="_blank">
+       
+      
           <img src={horizontal} className="belle" alt="bellevue logo" />
-        </a>
-        <div className={styles.background} />
-      </div>
-    
+   
+    </div>
+    </div>
   );
 }
 export default Education;
